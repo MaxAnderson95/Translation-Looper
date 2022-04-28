903634fcb2cd3e1f0591aa447661c5ad42e54a8f
Max Anderson <max.anderson@gucu.org>
2022-04-28 11:58:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Translation Looper
+This is a script that translates a phrase through a bunch of languages and then back to the source language to see how it changes.
+
+It uses the Microsoft Azure translation service and requires an API key to use.
+
+## Usage
+1. Clone the repo: ```git clone https://github.com/MaxAnderson95/Translation-Looper.git```
+2. Install the requirements ```pip install -r requirements.txt```
+3. Export an environment variable named `SUBSCRIPTION_KEY` with your API key from azure
+4. Export an environment variable named `LOCATION` with the Azure Region where your translation service is hosted. Ex. `eastus`
+5. Run the script:
+```commandline
+python .\main.py --phrase "Good morning!" --rounds 10
+Original phrase: Good morning
+
+Translating to Pashto => سهار مو پخیر
+Translating to Malay => Selamat pagi
+Translating to Icelandic => Góðan dag
+Translating to Divehi => ބާއްޖަވެރި މެންދުރެއް
+Translating to Tigrinya => ጽቡቕ እዋን
+Translating to Samoan => Taimi lelei
+Translating to Tigrinya => ፅቡቕ እዋን
+Translating to Inuinnaqtun => Algaaluarninga
+Translating to German => Handschuhe
+Translating to English => Gloves
+
+Final translation after 10 rounds => Gloves
+```
+
+If you don't specify a number of rounds, it will run 20 by default.
+
+You can also use a different source language other than English:
+```commandline
+python .\main.py --phrase "Bom dia" --source-language pt --rounds 10
+Original phrase: Bom dia
+
+Translating to Myanmar (Burmese) => မင်္ဂလာ နံနက်ခင်းပါ
+Translating to Marathi => शुभ प्रभात
+Translating to German => Guten Morgen
+Translating to Urdu => صبح بخير
+Translating to Khmer => អរុណសួស្តី
+Translating to Swedish => God morgon
+Translating to Haitian Creole => Bonjou
+Translating to Somali => Haye
+Translating to Hindi => सुनो
+Translating to Portuguese (Brazil) => Ei, ouça.
+
+Final translation after 10 rounds => Ei, ouça.
+```
---
+++ main.py
@@ -0,0 +1,100 @@
+import random
+import argparse
+import requests
+import uuid
+import os
+
+argparse = argparse.ArgumentParser()
+argparse.add_argument("--phrase", required=True)
+argparse.add_argument("--source-language", default='en')
+argparse.add_argument("--rounds", type=int, default=20)
+args = argparse.parse_args()
+phrase = args.phrase
+source = args.source_language
+rounds = args.rounds
+
+
+class Translator:
+    def __init__(self):
+        if "SUBSCRIPTION_KEY" not in os.environ:
+            raise Exception("Please export env var SUBSCRIPTION_KEY with your Azure Translation Service API key")
+        else:
+            subscription_key = os.environ.get("SUBSCRIPTION_KEY")
+
+        if "LOCATION" not in os.environ:
+            raise Exception("Please export env var LOCATION with your Azure Translation Service API key")
+        else:
+            location = os.environ.get("LOCATION")
+
+        self.headers = {
+            'Ocp-Apim-Subscription-Key': subscription_key,
+            'Ocp-Apim-Subscription-Region': location,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': str(uuid.uuid4())
+        }
+
+    base_url = "https://api.cognitive.microsofttranslator.com"
+
+    def translate(self, text: str, src: str, dest: str):
+        params = {
+            'api-version': '3.0',
+            'from': src,
+            'to': dest
+        }
+        body = [{
+            'text': text
+        }]
+
+        request = requests.post(self.base_url + '/translate', params=params, headers=self.headers, json=body)
+        response = request.json()
+        return response[0]["translations"][0]["text"]
+
+    @staticmethod
+    def get_supported_languages():
+        params = {
+            'api-version': '3.0',
+            'scope': 'translation'
+        }
+        request = requests.get(Translator.base_url + '/languages', params=params)
+        response = request.json()
+        return response['translation']
+
+    @staticmethod
+    def get_random_language():
+        choice = random.choice(list(Translator.get_supported_languages().keys()))
+        if choice == "tlh-Latn" or choice == "tlh-Piqd":  # Exclude Klingon because it completely destroys the meaning of the sentence
+            choice = Translator.get_random_language()
+        return choice
+
+
+def is_even(num: int):
+    return num % 2 == 0
+
+
+def rotate(translator: Translator, phrase: str, source: str, rounds: int):
+    last_translation = phrase
+    src = source
+    print(f"Original phrase: {phrase}")
+    print("")
+
+    for i in range(rounds):
+        if i == rounds - 1:  # If it's the final round, set the destination language to be the original source language.
+            language = source
+        else:
+            language = Translator.get_random_language()
+        last_translation = translator.translate(last_translation, src, language)
+        language_name = Translator.get_supported_languages()[language]["name"]
+        print(f"Translating to {language_name} => {last_translation}")
+        src = language  # Set the next source language to be the current iteration's destination language
+
+    print("")
+    print(f"Final translation after {rounds} rounds => {last_translation}")
+
+
+def main():
+    translator = Translator()
+    rotate(translator, phrase, source, rounds)
+
+
+if __name__ == "__main__":
+    main()
